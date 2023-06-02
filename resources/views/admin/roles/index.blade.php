d9de698959693997ce090c6f297ba9dbcc9845d7
Tihomir Manushev <tihomir.manushev@genericsoft.eu>
2023-06-02 21:38:57 +0300

    Display roles

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,9 +1,47 @@
 <x-admin-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Admin Role
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2">
+                <div class="flex justify-end p-2">
+                    <a href="" class="px-4 py-2 bg-green-700 hover:text-green-500 rounded-md">Create Role</a>
+                </div>
+                <div class="flex flex-col">
+                    <div class="my-2 overflow-x-auto sm:mx-6 lg:mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500">
+                                            Name
+                                        </th>
+                                        <th scope="col" class="relative px-6 py-3">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach($roles as $role)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    <div class="ml-4">
+                                                        <div
+                                                            class="text-sm font-medium text-gray-900">{{ $role->name }}</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="#" class="text-indigo-600 hover:text-gray-900">Edit</a>
+                                                <a href="#" class="text-indigo-600 hover:text-gray-900">Delete</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
