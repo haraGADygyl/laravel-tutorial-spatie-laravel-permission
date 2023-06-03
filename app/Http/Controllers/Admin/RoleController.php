ca9c2e3dc76d6990cc831b9df0d4f330105769d5
Tihomir Manushev <tihomir.manushev@genericsoft.eu>
2023-06-03 15:02:09 +0300

    Add delete role & permission

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -10,8 +10,43 @@ class RoleController extends Controller
 {
     public function index()
     {
-        $roles = Role::all();
+        $roles = Role::whereNotIn('name', ['admin'])->get();
 
         return view('admin.roles.index', compact('roles'));
+    }
+
+    public function create()
+    {
+        return view('admin.roles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate(['name' => 'required|min:3']);
+
+        Role::create($validated);
+
+        return redirect()->route('admin.roles.index')->with('message', 'Role Created successfully!');
+    }
+
+    public function edit(Role $role)
+    {
+        return view('admin.roles.edit', compact('role'));
+    }
+
+    public function update(Request $request, Role $role)
+    {
+        $validated = $request->validate(['name' => 'required|min:3']);
+
+        $role->update($validated);
+
+        return redirect()->route('admin.roles.index')->with('message', 'Role Updated successfully!');
+    }
+
+    public function destroy(Role $role)
+    {
+        $role->delete();
+
+        return back()->with('message', 'Role deleted');
     }
 }
